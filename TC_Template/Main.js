631294ed3095711c9d96169a5d5f7950d7191a55
dmarkovtsev <denis@inflectra.com>
2020-03-25 17:15:43 +0300

    Template added

---
+++ TC_Template/Main.js
@@ -0,0 +1,11 @@
+
+
+
+function Test(params)
+{
+	RVL.DoPlayScript("%WORKDIR%\\TC_Template\\Main.rvl.xlsx", "RVL");
+}
+
+g_load_libraries=["%g_browserLibrary%", "DomDynamicsAX"];
+
+
---
+++ TC_Template/Objects.js
@@ -0,0 +1,1 @@
+var saved_script_objects={};
