f93c3158e947df0f6fc28844822ec312db3f4285
dmarkovtsev <denis@inflectra.com>
2021-01-25 12:07:32 +0300

    CreateCustomerGroup TC

---
+++ TC_CreateCustomerGroup/Main.js
@@ -0,0 +1,11 @@
+
+
+
+function Test(params)
+{
+	RVL.DoPlayScript("%WORKDIR%\\TC_CreateCustomerGroup\\Main.rvl.xlsx", "RVL");
+}
+
+g_load_libraries=["%g_browserLibrary%", "DomDynamicsAX"];
+
+
---
+++ TC_CreateCustomerGroup/Objects.js
@@ -0,0 +1,106 @@
+var saved_script_objects={
+	"SystemDefinedNewButton": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "SystemDefinedNewButton",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Customer groups -- Finance and Operations",
+		"xpath": "//button[@data-dyn-controlname='SystemDefinedNewButton']",
+		"title": "Customer groups -- Finance and Operations",
+		"url": ""
+	},
+	"custgroup_Grid": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "DomDynamicsAXGrid",
+		"object_flavor": "Grid",
+		"object_name": "custgroup_Grid",
+		"ignore_object_name": true,
+		"object_library": "DomDynamicsAX",
+		"window_name": "Customer groups -- Finance and Operations",
+		"xpath": "//div[contains(@id,'custgroup') and contains(@id,'Grid') and @role='grid']",
+		"title": "Customer groups -- Finance and Operations",
+		"url": ""
+	},
+	"SystemDefinedSaveButton": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "SystemDefinedSaveButton",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Customer groups -- Finance and Operations",
+		"xpath": "//button[@data-dyn-controlname='SystemDefinedSaveButton']",
+		"title": "Customer groups -- Finance and Operations",
+		"url": ""
+	},
+	"NavBarDashboard": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "NavBarDashboard",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Customer groups -- Finance and Operations",
+		"xpath": "//button[@data-dyn-controlname='NavBarDashboard']",
+		"title": "Customer groups -- Finance and Operations",
+		"url": ""
+	}
+};
